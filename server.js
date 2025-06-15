929432eae3c9e5efafec1fbfda314422702c271f
Donut-Doctor <palsuman174@gmail.com>
2025-06-15 13:03:17 +0530

    minor changes in server.js

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = 3004;
 
-
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
 
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -20,7 +17,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
